5ac736a7ddd6fe3e049689471aaeac79814690d7
sgawas3 <sgawas3@gatech.edu>
2017-03-27 23:57:39 -0400

    Moved to github

--- app.py
+++ app.py
@@ -145,6 +145,15 @@ def request_postRequest():
     db.session.commit()
     return ''
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        f_name = str(uuid.uuid4()) + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        return json.dumps({'filename':f_name})
+
 def error_msg(param):
     err_j = {}
     err_j['Error'] = param
