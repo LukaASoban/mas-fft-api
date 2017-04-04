62873caac6fc2061bdf63ab3cfd8d52032639259
Agarwal, Akshay <aagarwal316@gatech.edu>
2017-04-03 23:03:45 -0400

    updated deprecated libraries

--- app_init.py
+++ app_init.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
-from flask.ext.cors import CORS, cross_origin
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADER'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fft_db:fftslam2017@fft-db2.cmridtwsayjd.us-east-1.rds.amazonaws.com:5432/postgres'
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fft_db:fftslam2017@fft-db2.cmridtwsayjd.us-east-1.rds.amazonaws.com:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['WTF_CSRF_ENABLED'] = False
 db = SQLAlchemy(app)
 
