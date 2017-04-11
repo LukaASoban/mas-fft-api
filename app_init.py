4b14d3ad7bff8e4ea8cd76063d7d20f4bd78ea27
Agarwal, Akshay <aagarwal316@gatech.edu>
2017-04-10 20:30:39 -0400

    commented import statements

--- app_init.py
+++ app_init.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 #from model import db
-from model import User
-from model import Share
-from model import transportRequests
-from model import Request
+#from model import User
+#from model import Share
+#from model import transportRequests
+#from model import Request
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,7 +15,7 @@ app.config['CORS_HEADER'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['WTF_CSRF_ENABLED'] = False
 db = SQLAlchemy(app)
-
+db.create_all()
 with app.app_context():
     db.create_all()
 
