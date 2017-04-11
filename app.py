78832dd0f95de7c9724462349c2511632fea8d59
Agarwal, Akshay <aagarwal316@gatech.edu>
2017-04-11 03:15:08 -0400

    returning transport_id

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def transport_createRequest():
         query2 = db.session.query(Share).filter(Share.share_id == temp['share_id'])
         query2.share_status = 'matched'
     db.session.commit()
-    return "Success"
+    return json.dumps(transport.to_json())
 
 
 #api endpoint for viewing all transport requests
