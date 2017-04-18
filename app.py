60d16fcaed35f824be7c9eed1c8ca305fbcca416
Agarwal, Akshay <aagarwal316@gatech.edu>
2017-04-18 01:30:51 -0400

    debug

--- app.py
+++ app.py
@@ -240,13 +240,17 @@ def transport_acceptRequest():
 
     query3 = db.session.query(User).filter(User.user_id ==temp['request_user_id'] )
     payload['tokens'][0] = query3[0].token
+    print payload['tokens']
     response = requests.request("POST", url, data=payload, headers=headers)
 
 
     query3 = db.session.query(Share).filter(Share.share_id ==temp['share_id'] )
     query4 = db.session.query(User).filter(User.user_id ==query3[0].user_id )
     payload['tokens'][0] = query4[0].token
+    print payload['tokens']
     response = requests.request("POST", url, data=payload, headers=headers)
+    print "RESPONSE",
+    print response
     # query2 = db.session.query(Share).filter(Share.share_id == temp['share_id'])
     # query2.share_status = 'matched'
 
